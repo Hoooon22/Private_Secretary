279e2b2acce44acfcc2af5557a0423adcd6f493b
Hoooon22 <momo990305@gmail.com>
2021-12-06 21:08:44 +0900

    create Voice composition

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
 namespace Private_Secretary
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 음성 SDK를 사용하여 음성 서비스를 호출
+            var speechConfig = SpeechConfig.FromSubscription("<paste-your-speech-key-here>", "<paste-your-speech-location/region-here>");
         }
     }
 }
