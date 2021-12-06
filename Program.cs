7ed79293057c4e4487707cd472928d839a530dd3
Hoooon22 <momo990305@gmail.com>
2021-12-06 18:28:43 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Private_Secretary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
