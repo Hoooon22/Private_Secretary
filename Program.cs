48d893edd63aea39eaf9d21556a617048c382559
Hoooon22 <momo990305@gmail.com>
2021-12-08 01:14:34 +0900

    insert key and region

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ namespace Private_Secretary
         async static Task Main(string[] args)
         {
             // 음성 SDK를 사용하여 음성 서비스를 호출
-            var speechConfig = SpeechConfig.FromSubscription("<paste-your-speech-key-here>", "<paste-your-speech-location/region-here>");
+            var speechConfig = SpeechConfig.FromSubscription("e1d9172eea8241a7aa993ed74df9b886", "koreacentral");
             await FromMic(speechConfig);
         }
     }
