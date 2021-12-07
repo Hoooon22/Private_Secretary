180433d63474029429d36f732d3050103bb5b6fc
Hoooon22 <momo990305@gmail.com>
2021-12-08 02:35:35 +0900

    sync

--- Program.cs
+++ Program.cs
@@ -47,7 +47,7 @@ namespace Private_Secretary
             // 음성 SDK를 사용하여 음성 서비스를 호출
             var speechConfig = SpeechConfig.FromSubscription("e1d9172eea8241a7aa993ed74df9b886", "koreacentral");
 
-            speech_str =  await FromMic(speechConfig);
+            speech_str = await FromMic(speechConfig);
             Console.WriteLine($"speech_str Test : {speech_str}");
         }
     }
