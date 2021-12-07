4508986a730a2048523dc31cfa590b45662fb702
Hoooon22 <momo990305@gmail.com>
2021-12-07 15:45:52 +0900

    FromMic

--- Program.cs
+++ Program.cs
@@ -10,13 +10,40 @@ namespace Private_Secretary
     {
         async static Task FromMic(SpeechConfig speechConfig)
         {
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
+            Console.WriteLine("Speak into your microphone.");
+            var result = await recognizer.RecognizeOnceAsync();
+            Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+            // error
+            switch (result.Reason)
+            {
+                case ResultReason.RecognizedSpeech:
+                    Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+                    break;
+                case ResultReason.NoMatch:
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the speech key and location/region info?");
+                    }
+                    break;
+            }
         }
 
         async static Task Main(string[] args)
         {
             // 음성 SDK를 사용하여 음성 서비스를 호출
             var speechConfig = SpeechConfig.FromSubscription("<paste-your-speech-key-here>", "<paste-your-speech-location/region-here>");
+            await FromMic(speechConfig);
         }
     }
 }
