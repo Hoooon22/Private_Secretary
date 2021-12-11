84869683469a945f5493c41a9bb23542d651fb1f
지훈 김 <momo990305@gmail.com>
2021-12-11 20:31:12 +0900

    add searchBox, searchButton

--- Program.cs
+++ Program.cs
@@ -58,6 +58,8 @@ namespace Private_Secretary
                     {
                         // connect URL
                         cDriver.Url = "https://www.google.com";
+                        var searchBox = cDriver.FindElement(By.XPath("//*[@id='input']"));
+                        var searchButton = cDriver.FindElement(By.XPath("//*[@id='icon']"));
                     }
                 }
                 catch (Exception e)
